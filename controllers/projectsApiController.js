cb52c45767e17b5c101175265b10b41fa59eebf6
JorgeMonterde <j.monterdeortega@gmail.com>
2023-06-20 17:58:44 +0200

    comment/some_lines

--- controllers/projectsApiController.js
+++ controllers/projectsApiController.js
@@ -1,7 +1,22 @@
+const Project = require('../models/projects'); // Models from Mongo(Atlas);
+
 //GETs
 // search results list (user and admin)
-const getSearchResults = () => {
-
+const getSearchResults = async (req,res) => {
+    let {title} = req.query;
+    try {
+        let projects = await Project.find({"title":title},'-_id -__v'); //[{}]
+        if(projects[0]){
+            res.status(200).json(projects); // Respuesta de la API para 1 producto {}
+        } else {
+            res.status(200).json({}); // Respuesta de la API cuando no hay productos
+        }
+    } catch (error) {
+        console.log(`ERROR: ${error.stack}`);
+            res.status(400).json({
+                msj: `ERROR: ${error}`
+        });
+    }
 };
 
 //POSTs
