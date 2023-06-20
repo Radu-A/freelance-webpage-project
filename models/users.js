525a9296a5c3631533c90d260ae7544fd31e8ca6
Radu-A <elcastravete@gmail.com>
2023-06-20 21:50:30 +0200

    added/users_and_users.queries.js

--- models/users.js
+++ models/users.js
@@ -1,18 +1,12 @@
 const pool = require('../utils/db_pgsql');
+const usersQueries = require('../queries/users.queries');
 
+// This will be used when "admin" load "users view"
 const getAllUsers = async () => {
     let client, result;
     try {
         client = await pool.connect();
-        const data = await client.query(
-            `SELECT 
-                email, 
-                password, 
-                user_name, 
-                admin, 
-                firstname, 
-                surename
-            FROM public.users;`);
+        const data = await client.query(usersQueries.getAllUsers);
         result = data.rows;
         console.log(result);
     } catch(err) {
@@ -24,22 +18,12 @@ const getAllUsers = async () => {
     return result;
 }
 
+// It will be need it... I dont know
 const getUsersById = async (id_user) => {
     let client, result;
     try {
         client = await pool.connect();
-        let data = await client.query(
-            `SELECT 
-                id_user, 
-                email, 
-                password, 
-                user_name, 
-                admin, 
-                firstname, 
-                surename
-            FROM public.users
-            WHERE id_user = $1`,
-            [id_user]);
+        let data = await client.query(usersQueries.getUserById, [id_user]);
         result = data.rows;
         console.log(result);
     } catch(err) {
@@ -51,20 +35,51 @@ const getUsersById = async (id_user) => {
     return result
 }
 
+// Users will be created in the sign in
 const createUser = async(email, password, user_name, admin, firstname, surename) => {
     let client, result;
     try{
         client = await pool.connect();
-        const data = await client.query(`
-        INSERT INTO users(
-            email, 
-            password, 
-            user_name, 
-            admin, 
-            firstname, 
-            surename)
-        VALUES ($1, $2, $3, $4, $5, $6)`,
+        const data = await client.query(usersQueries.createUser,
         [email, password, user_name, admin, firstname, surename]);
+        result = data.rowCount;
+        console.log(result);
+    }catch(err){
+        console.log(err);
+        throw(err);
+    }finally{
+        client.release()
+    }
+    return result
+};
+
+// Update user will be done by users in their profile page
+const updateUser = async(id_user, email, password, user_name, firstname, surename) => {
+    let client, result;
+    try{
+        client = await pool.connect();
+        // Admin field is not here cause the user is not allowed to change it
+        const data = await client.query(usersQueries.updateUser,
+        [id_user, email, password, user_name, firstname, surename]);
+        result = data.rowCount;
+        console.log(result);
+    }catch(err){
+        console.log(err);
+        throw(err);
+    }finally{
+        client.release()
+    }
+    return result
+};
+
+// Update user will be done by admin in "users view"
+const deleteUser = async(id_user) => {
+    let client, result;
+    try{
+        client = await pool.connect();
+        // Admin field is not here cause the user is not allowed to change it
+        const data = await client.query(usersQueries.deleteUser,
+        [id_user]);
         result = data.rowCount;
         console.log(result);
     }catch(err){
@@ -79,4 +94,14 @@ const createUser = async(email, password, user_name, admin, firstname, surename)
 // createUser('admin@gmail.com', 'abc123', 'admin', true, 'john', 'doe');
 // createUser('user@gmail.com', 'abc123', 'user', false, 'jane', 'dee');
 // getAllUsers()
-// getUsersById(2)
+// getUsersById(6)
+// updateUser(6, 'jane@gmail.com', 'abc123', false, 'jane', 'dee');
+// deleteUser(4)
+
+module.exports = {
+    getAllUsers,
+    getUsersById,
+    createUser,
+    updateUser,
+    deleteUser
+}
