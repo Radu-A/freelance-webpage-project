a94a849b83b3323574214c2f6d71833c747723ae
Radu-A <elcastravete@gmail.com>
2023-06-22 00:21:08 +0200

    added/dashboard

--- public/script.js
+++ public/script.js
@@ -25,65 +25,68 @@ async function putData(url = "", data = {}) {
 const formUserData = document.getElementById("formUserData");
 const editUserData = document.getElementById("editUserData");
 
-const form = document.createElement("form");
-  const emailInput = document.createElement("input");
-  const userNameInput = document.createElement("input");
-  const firstNameInput = document.createElement("input");
-  const surnameInput = document.createElement("input");
-  const submitButton = document.createElement("button");
+if (formUserData) {
+	const form = document.createElement("form");
+  	const emailInput = document.createElement("input");
+	const userNameInput = document.createElement("input");
+	const firstNameInput = document.createElement("input");
+	const surnameInput = document.createElement("input");
+	const submitButton = document.createElement("button");
 
-  emailInput.type = "email";
-  emailInput.placeholder = "Email";
-  emailInput.name = "email";
-  userNameInput.type = "text";
-  userNameInput.placeholder = "User Name";
-  userNameInput.name = "userName";
-  firstNameInput.type = "text";
-  firstNameInput.placeholder = "First Name";
-  firstNameInput.name = "firstName";
-  surnameInput.type = "text";
-  surnameInput.placeholder = "Surname";
-  surnameInput.name = "surname";
+	emailInput.type = "email";
+	emailInput.placeholder = "Email";
+	emailInput.name = "email";
+	userNameInput.type = "text";
+	userNameInput.placeholder = "User Name";
+	userNameInput.name = "userName";
+	firstNameInput.type = "text";
+	firstNameInput.placeholder = "First Name";
+	firstNameInput.name = "firstName";
+	surnameInput.type = "text";
+	surnameInput.placeholder = "Surname";
+	surnameInput.name = "surname";
 
-  submitButton.type = "submit";
-  submitButton.innerText = "Save edit";
+	submitButton.type = "submit";
+	submitButton.innerText = "Save edit";
 
-  form.appendChild(emailInput);
-  form.appendChild(userNameInput);
-  form.appendChild(firstNameInput);
-  form.appendChild(surnameInput);
-  form.appendChild(submitButton);
+	form.appendChild(emailInput);
+	form.appendChild(userNameInput);
+	form.appendChild(firstNameInput);
+	form.appendChild(surnameInput);
+	form.appendChild(submitButton);
 
-  formUserData.appendChild(form);
+	formUserData.appendChild(form);
 
-  // Send user info event
-  submitButton.addEventListener("submit", function(e) {
-    e.preventDefault();
-    
-    let newInfo = {
-        email: e.target.email.value,
-        userName: e.target.userName.value,
-        firstName: e.target.firstName.value,
-        surename: e.target.surname.value
-    }
-    console.log("+++++++",newInfo)
+	// Send user info event
+	submitButton.addEventListener("submit", function(e) {
+		e.preventDefault();
+		
+		let newInfo = {
+			email: e.target.email.value,
+			userName: e.target.userName.value,
+			firstName: e.target.firstName.value,
+			surename: e.target.surname.value
+		}
+		console.log("+++++++",newInfo)
 
-    try {
-        putData("http://localhost:3000/api/users/user", newInfo).then((data) => {
-        console.log(data); // JSON data parsed by `data.json()` call
-        }); 
-    } catch (error) {
-        console.error(error);
-    }
-  });
-/*   
-editUserData.addEventListener("click", function(event) {
-    event.preventDefault();
+		try {
+			putData("http://localhost:3000/api/users/user", newInfo).then((data) => {
+			console.log(data); // JSON data parsed by `data.json()` call
+			}); 
+		} catch (error) {
+			console.error(error);
+		}
+	});
+	/*   
+	editUserData.addEventListener("click", function(event) {
+		event.preventDefault();
 
-  
+	
 
-});
- */
+	});
+	*/
+}
+
 
 
 
@@ -142,11 +145,20 @@ async function getProjects(keyword) {
 		}
 	}
 }
+if (searchProjectsForm) {
+	searchProjectsForm.addEventListener('submit', (event) => {
+		event.preventDefault();
+	
+		searchResultsSection.innerHTML = '';
+		const keywordInput = document.querySelector('#keyword');
+		getProjects(keywordInput.value);
+	})
+}
 
-searchProjectsForm.addEventListener('submit', (event) => {
-	event.preventDefault();
 
-	searchResultsSection.innerHTML = '';
-	const keywordInput = document.querySelector('#keyword');
-	getProjects(keywordInput.value);
-})
+
+// -------------------DASHBOARD-------------------------------
+const dashboardResultsSection = document.querySelector('.dashboard');
+if (dashboardResultsSection) {
+	getProjects();
+}
