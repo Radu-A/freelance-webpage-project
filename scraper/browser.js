09e9846959dd36e906ad4323f34cd04a6ee67cc8
Radu-A <elcastravete@gmail.com>
2023-06-28 11:31:43 +0200

    dockerfile

--- scraper/browser.js
+++ scraper/browser.js
@@ -1,12 +1,21 @@
 const puppeteer = require('puppeteer');
+require('dotenv').config();
 
 async function startBrowser(){
     let browser;
     try {
         console.log("Opening the browser.....");
-        browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-            'ignoreHTTPSErrors': true
+        browser = await puppeteer.launch(
+            {   args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+            ],
+                executablePath:
+                process.env.NODE_ENV === "production"
+                ? process.env.PUPPETEER_EXECUTABLE_PATH
+                : puppeteer.executablePath()  
         })
     } catch (error) {
         console.log("Could not create a browser instance => : ", error);
